f08ce273ca3831c56eb3d1c0c9cafa5fb0148980
syx <syx_ouyuchen@sina.com>
2021-07-27 10:50:08 +0800

    33.搜索旋转排序数组

--- src/main/java/syx/july2021/The1thDay.java
+++ src/main/java/syx/july2021/The1thDay.java
@@ -1,9 +1,11 @@
 package syx.july2021;
 
+import org.junit.Test;
+
 /**
  * @author syx
  * @className The1thDay
- * @description TODO
+ * @description 标签：二分查找
  * @date 2021/07/26 17:06
  */
 public class The1thDay {
@@ -47,5 +49,67 @@ public class The1thDay {
             }
         }
         return result;
+    }
+
+    /**
+     * 题目：33. 搜索旋转排序数组
+     * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+     */
+    public int search1(int[] nums, int target) {
+        int result = -1;
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int length = nums.length;
+        if (nums[0] > target && nums[length - 1] < target) {
+            return result;
+        }
+
+        int left = 0;
+        int right = nums.length - 1, middle;
+        int first = nums[0];
+        int last = nums[right];
+        while (left <= right) {
+            middle = (left + right) / 2;
+            // 比较中间位置的值
+            if (nums[middle] == target) {
+                return middle;
+            }
+            // 判断左右哪方属于顺序
+            if (first <= nums[middle]) {
+                // 左边顺序
+                if (first <= target && nums[middle] > target) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            } else {
+                // 右边顺序
+                if (last >= target && nums[middle] < target) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int search2(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    @Test
+    public void test() {
+        search1(new int[]{4, 5, 6, 7, 0, 1, 2}, 0);
     }
 }
