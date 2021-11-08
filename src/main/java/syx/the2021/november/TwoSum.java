6afc33de2e5badac8ab9640a035c12aa6c6ac59f
syx <syx_ouyuchen@sina.com>
2021-11-08 17:01:10 +0800

    167.两数之和 II 输入有序数组

--- src/main/java/syx/the2021/november/TwoSum.java
+++ src/main/java/syx/the2021/november/TwoSum.java
@@ -31,5 +31,35 @@ public class TwoSum {
         }
         return result;
     }
+}
 
+/**
+ * https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+ * 167.两数之和 II 输入有序数组
+ *
+ * @tips 数组、双指针、二分查找
+ * @date 2021/11/08 16:23
+ */
+class InputArraySort {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{};
+        }
+        int[] result = new int[2];
+        // 输入的数组为有序
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            // 左侧+右侧和target相比较
+            if (nums[left] + nums[right] == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            if (nums[left] + nums[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return result;
+    }
 }
