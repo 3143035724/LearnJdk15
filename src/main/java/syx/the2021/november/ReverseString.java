3590b2ff378b6cf1454a4196b55747dea06fff45
syx <syx_ouyuchen@sina.com>
2021-11-12 16:45:15 +0800

    344.反转字符串

---
+++ src/main/java/syx/the2021/november/ReverseString.java
@@ -0,0 +1,28 @@
+package syx.the2021.november;
+
+/**
+ * @author syx
+ * @className ReverseString
+ * @description Write a function that reverses a string. The input string is given as an array of characters s
+ * You must do this by modifying the input array in-place with O(1) extra memory
+ * @url https://leetcode-cn.com/problems/reverse-string/
+ * @tips Recursion、TwoPointers、String
+ * @date 2021/11/12 16:37
+ */
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        if (s == null || s.length == 0) {
+            return;
+        }
+        // 反转数组
+        int length = s.length;
+        int middle = length / 2;
+        for (int i = 0; i < middle; i++) {
+            char temp = s[length - 1 - i];
+            s[length - 1 - i] = s[i];
+            s[i] = temp;
+        }
+    }
+
+}
