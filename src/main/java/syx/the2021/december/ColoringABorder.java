9f9bc76309a09601994a54dc4e826ca3d84afca0
syx <syx_ouyuchen@sina.com>
2021-12-07 17:57:46 +0800

    1034 边界着色 类定义

---
+++ src/main/java/syx/the2021/december/ColoringABorder.java
@@ -0,0 +1,24 @@
+package syx.the2021.december;
+
+/**
+ * @author syx
+ * @className ColoringABorder
+ * @description 1034 边界着色
+ * @url https://leetcode-cn.com/problems/coloring-a-border/
+ * @tips Depth-First Search、Breadth-First Search、Array、Matrix
+ * @date 2021/12/07 17:20
+ */
+public class ColoringABorder {
+
+    /**
+     * 给你一个大小为 m x n 的整数矩阵 grid ，表示一个网格。另给你三个整数 row、col 和 color 。网格中的每个值表示该位置处的网格块的颜色。
+     * 当两个网格块的颜色相同，而且在四个方向中任意一个方向上相邻时，它们属于同一 连通分量 。
+     * 连通分量的边界 是指连通分量中的所有与不在分量中的网格块相邻（四个方向上）的所有网格块，或者在网格的边界上（第一行/列或最后一行/列）的所有网格块。
+     * 请你使用指定颜色 color 为所有包含网格块 grid[row][col] 的 连通分量的边界 进行着色，并返回最终的网格 grid
+     */
+    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        // TODO
+        System.out.println("定义该方法");
+        return null;
+    }
+}
