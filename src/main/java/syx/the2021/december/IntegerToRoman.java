57bb9100fa297639909503abc57a893e30f8d340
syx <syx_ouyuchen@sina.com>
2021-12-08 09:37:44 +0800

    12.整数转罗马数字

---
+++ src/main/java/syx/the2021/december/IntegerToRoman.java
@@ -0,0 +1,33 @@
+package syx.the2021.december;
+
+/**
+ * @author syx
+ * @className IntegerToRoman
+ * @description 整数转罗马数字
+ * @url https://leetcode-cn.com/problems/integer-to-roman/
+ * @tips HashTable、Math、String
+ * @date 2021/12/07 17:59
+ */
+public class IntegerToRoman {
+
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+
+    public String intToRoman(int num) {
+        // 判断num所在区间
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            int value = values[i];
+            String symbol = symbols[i];
+            while (num >= value) {
+                num -= value;
+                result.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        return result.toString();
+    }
+}
