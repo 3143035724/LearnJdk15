70066844aa0e88caaa79df0701667b1239e805c9
syx <syx_ouyuchen@sina.com>
2021-12-17 16:54:13 +0800

    162.寻找峰值

---
+++ src/main/java/syx/the2021/december/FindPeakElement.java
@@ -0,0 +1,28 @@
+package syx.the2021.december;
+
+/**
+ * @author syx
+ * @className FindPeakElement
+ * @description 寻找数组中峰值对应索引
+ * @url https://leetcode-cn.com/problems/find-peak-element/
+ * @tips Array、Binary Search
+ * @date 2021/12/17 16:33
+ */
+public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return 0;
+        }
+        if (length == 2) {
+            return nums[0] > nums[1] ? 0 : 1;
+        }
+        for (int i = 1; i < length - 1; i++) {
+            if (nums[i - 1] < nums[i] && nums[i + 1] < nums[i]) {
+                return i;
+            }
+        }
+        return nums[0] > nums[length - 1] ? 0 : length - 1;
+    }
+}
