30f8f313208e0e5e707e832da858238c1ad0e09b
syx <syx_ouyuchen@sina.com>
2021-12-17 17:04:50 +0800

    无用的优化

--- src/main/java/syx/the2021/december/FindPeakElement.java
+++ src/main/java/syx/the2021/december/FindPeakElement.java
@@ -15,9 +15,6 @@ public class FindPeakElement {
         if (length == 1) {
             return 0;
         }
-        if (length == 2) {
-            return nums[0] > nums[1] ? 0 : 1;
-        }
         for (int i = 1; i < length - 1; i++) {
             if (nums[i - 1] < nums[i] && nums[i + 1] < nums[i]) {
                 return i;
