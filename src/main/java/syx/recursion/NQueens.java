5d84b4c956e78541d7285591a50a5d66d9927a07
syx <syx_ouyuchen@sina.com>
2021-12-20 13:48:10 +0800

    八皇后类提交(未实现)

---
+++ src/main/java/syx/recursion/NQueens.java
@@ -0,0 +1,25 @@
+package syx.recursion;
+
+import java.util.List;
+
+/**
+ * @author syx
+ * @className NQueens
+ * @description N Queens
+ * @url https://leetcode-cn.com/problems/n-queens/
+ * @tips Arrays、Backtracking
+ * @date 2021/12/20 10:31
+ */
+public class NQueens {
+    // TODO 八皇后或N皇后的实现
+
+    public List<List<String>> solveNQueens(int n) {
+        // 递归、回溯算法
+        // n代表皇后的个数，并代表棋盘的大小
+        int[][] arrays = new int[n][n];
+        // 假如第一个皇后在第一个位置，那么同一行，同一列以及同一斜线均不能放皇后
+
+        return null;
+    }
+
+}
